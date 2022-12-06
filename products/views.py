eefea445e68a14d825eae6998fc07f5be315de45
behradbhrmi <behrad.bhrmi@gmail.com>
2022-12-06 16:05:58 +0330

    add few views to views.py

--- products/views.py
+++ products/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import get_object_or_404
 
-# Create yor views here.
+from .models import Products, Comments
+from .forms import CommentForm
 
 
-def home(request):
-    return HttpResponse('Hello')
+class ProductsListView(ListView):
+    queryset = Products.objects.filter(availability=True)
+    context_object_name = 'products'
+    template_name = 'products/products_list.html'
+
+
+class ProductDetailView(DetailView):
+    model = Products
+    context_object_name = 'product'
+    template_name = 'products/product_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+
+class CommentCreateView(CreateView):
+    model = Comments
+    form_class = CommentForm
+    template_name = ProductDetailView.template_name
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.user = self.request.user
+        pk = int(self.kwargs['pk'])
+        product = get_object_or_404(Products, id=pk)
+        obj.product = product
+        return super().form_valid(form)
