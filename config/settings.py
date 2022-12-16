250cf6e143b1b4791b9e10c6634b346c5e8036b1
behradbhrmi <behrad.bhrmi@gmail.com>
2022-12-16 20:44:03 +0330

    add messages config

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants as mc
 from pathlib import Path
 from environs import Env
 
@@ -176,3 +177,8 @@ LOGOUT_REDIRECT_URL = 'home'
 
 # crispy settings
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# messages setting
+MESSAGE_TAGS = {
+    mc.ERROR: 'danger',
+}
